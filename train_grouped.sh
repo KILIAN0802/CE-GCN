a2ad0d3872c35ee2f15d363764c20aaaaf1220bc
nguyendat <nguyendat08022004@gmail.com>
2026-05-14 09:53:56 +0700

    train_grouped.bash

--- train_grouped.sh
+++ train_grouped.sh
@@ -4,26 +4,26 @@
 WANDB_RUN_ID=$(python -c "import uuid; print(uuid.uuid4())")
 
 # 1. Train Joint model
-echo "--- Training Joint Model ---"
-python -m trainer.trainer --config configs/transfer_joint.yaml --wandb.id $WANDB_RUN_ID --wandb.group "transfer-learning" --wandb.enable True
+# echo "--- Training Joint Model ---"
+# python -m trainer.trainer --config configs/transfer_joint.yaml --wandb.id $WANDB_RUN_ID --wandb.group "transfer-learning" --wandb.enable True
 
 # Check if the first command was successful
-if [ $? -ne 0 ]; then
-    echo "Joint model training failed. Exiting."
-    exit 1
-fi
+# if [ $? -ne 0 ]; then
+#     echo "Joint model training failed. Exiting."
+#     exit 1
+# fi
 
 echo "--- Joint Model Training Finished ---"
 
 # The best model path from the joint training
-BEST_MODEL_PATH="./results/transfer_joint/best_model.pth"
+BEST_MODEL_PATH="./results/noJDMA/transfer_joint/best_model.pth"
 
 # 2. Train Bone model
 echo "--- Training Bone Model ---"
 # Update the pretrained_path in the bone config before running
 # This is a temporary modification for the run
 TARGET_STREAM=BONE python -m trainer.trainer --config configs/transfer_bone.yaml \
-    --model_args.pretrained_path $BEST_MODEL_PATH \
+    --pretrained_path $BEST_MODEL_PATH \
     --wandb.id $WANDB_RUN_ID --wandb.group "transfer-learning" --wandb.enable True --wandb.resume allow
 
 if [ $? -ne 0 ]; then
@@ -38,7 +38,7 @@ echo "--- Bone Model Training Finished ---"
 echo "--- Training Velocity Model ---"
 # Update the pretrained_path in the velocity config before running
 TARGET_STREAM=VELOCITY python -m trainer.trainer --config configs/transfer_vel.yaml \
-    --model_args.pretrained_path $BEST_MODEL_PATH \
+    --pretrained_path $BEST_MODEL_PATH \
     --wandb.id $WANDB_RUN_ID --wandb.group "transfer-learning" --wandb.enable True --wandb.resume allow
 
 if [ $? -ne 0 ]; then
